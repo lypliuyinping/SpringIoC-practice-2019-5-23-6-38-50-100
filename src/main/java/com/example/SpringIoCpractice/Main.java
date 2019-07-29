e7765b2ce6596afb0a704c8a81f6935a1c7bc1fa
liuyinping <1689372042@qq.com>
2019-07-29 17:07:35 +0800

    修改hello、main

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,16 @@
+package com.example.SpringIoCpractice;
+
+import com.example.SpringIoCpractice.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
 //TODO
+@Component
+public class Main {
+    public void testMain(){
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("hello.xml");
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("HelloWorldBean");
+        helloWorld.getHello();
+    }
+}
